3d45acbe50e00de9171e543e9a23e86052709957
Biran <bndiaye@isi-mtl.com>
2021-01-17 11:47:57 -0500

    serch component

--- tp/src/component/playlist-select.js
+++ tp/src/component/playlist-select.js
@@ -3,11 +3,11 @@ import React from 'react'
 function optionBuider (option, index) {
     return <option key={index} value={option}>{option}</option>
 }
-const PlaylistSelect = ({ name, id, options, onClick }) => {
+const PlaylistSelect = ({ name, id, options, onClick }) => (
     <div>
         <select onClick={onClick} className='custom-select' name={name} id={id}>
             {options.map((option, index) => optionBuider(option, index))}
         </select>
     </div>
-}
+)
 export default PlaylistSelect
---
+++ tp/src/component/search-input.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const SearchInput = ({ type, id, name, placeholder }) => (
+    <input type={type} name={name} id={id} placeholder={placeholder} />
+)
+export default SearchInput
