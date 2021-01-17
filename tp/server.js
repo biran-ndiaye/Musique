0097d72f09f743ce723f5a2cfd2dbc9980c8d9e3
Biran <bndiaye@isi-mtl.com>
2021-01-17 01:22:50 -0500

    configuration server

--- tp/server.js
+++ tp/server.js
@@ -1,1 +1,35 @@
 
+'use strict'
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(express.static('dist'))
+// parse application/json
+app.use(bodyParser.json())
+// CORS for development
+// https://enable-cors.org/server_expressjs.html
+app.use(function (request, response, next) {
+    response.header('Access-Control-Allow-Origin', '*')
+    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    response.header('Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE, OPTIONS')
+    response.header('Access-Control-Allow-Credentials', 'false')
+    next()
+})
+
+const PORT = 8080
+const HTTP_OK = 200
+const CONTENT_TYPE_JSON = 'application/json'
+
+function sendResponse (response, datas) {
+    response.writeHead(HTTP_OK, { 'content-type': CONTENT_TYPE_JSON })
+    response.end(datas)
+}
+
+app.listen(PORT, function () {
+    console.log('Server listening on: http://localhost:%s', PORT)
+})
