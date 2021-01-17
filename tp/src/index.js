d9a77400df3baafc0762d3522cc59a066cf99160
Biran <bndiaye@isi-mtl.com>
2021-01-17 01:47:43 -0500

    architecture projet

---
+++ tp/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import MusicContainer from 'container/crud-container'
+
+ReactDOM.render(
+    <MusicContainer />,
+    document.getElementById('app')
+)
